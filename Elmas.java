ce62fe6b6a011c2d884160641a17567b312c8602
Ahmet Faruk TÜRK <af_trk@hotmail.com>
2023-04-20 23:43:31 +0300

    Elmas.java added

---
+++ Elmas.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Elmas {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Sayı giriniz: ");
+		n = scan.nextInt();
+		
+		for(int i = 1; i<= n; i++) {
+			for(int j = 1; j <= (n-i); j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=(2*i)-1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i=1; i<=n-1; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*(n-i)-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+}
